fbefc84ae40995b2aa3c027e0e693694e97a611b
TalhaUmair1 <mtalhaumair014@gmail.com>
2025-09-25 12:22:04 +0500

    feat: add the link button

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -2,52 +2,19 @@
 import type { NavigationMenuItem } from '@nuxt/ui'
 
 const route = useRoute()
-
-const items = computed<NavigationMenuItem[]>(() => [
-  {
-    label: 'Docs',
-    to: '/docs/getting-started',
-    active: route.path.startsWith('/docs/getting-started')
-  },
-  {
-    label: 'Components',
-    to: '/docs/components',
-    active: route.path.startsWith('/docs/components')
-  },
-  {
-    label: 'Figma',
-    to: 'https://go.nuxt.com/figma-ui',
-    target: '_blank'
-  },
-  {
-    label: 'Releases',
-    to: 'https://github.com/nuxt/ui/releases',
-    target: '_blank'
-  }
-])
 </script>
 
 <template>
   <UHeader>
-    <template #title>
-      <Logo class="h-6 w-auto" />
+    <template #left>
+      <NuxtLink to="/">
+        <UHeader title="Nuxt UI" />
+      </NuxtLink>
     </template>
-
-    <UNavigationMenu :items="items" />
-
     <template #right>
+      <ULink class="bg-primary text-neutral px-4 py-1 rounded-sm" as="button">Sign up</ULink>
       <UColorModeButton />
-
-      <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
-        <UButton
-          color="neutral"
-          variant="ghost"
-          to="https://github.com/nuxt/ui"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-        />
-      </UTooltip>
     </template>
   </UHeader>
 </template>
+
