51c70f07868a841bfdf9e4c851b0d0e4591fc615
TalhaUmair1 <mtalhaumair014@gmail.com>
2025-09-25 14:55:33 +0500

    feat : add the data base

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,10 +1,14 @@
-import { drizzle } from 'drizzle-orm/libsql'
-
 import env from '~/lib/env'
-// You can specify any property from the libsql connection options
+
+import * as schema from './schema'
+
+import { drizzle } from 'drizzle-orm/libsql'
 const db = drizzle({
   connection: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
   },
+  schema,
 })
+
+export default db
---
+++ lib/db/schema/location.ts
@@ -0,0 +1,8 @@
+import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+
+export const locations = sqliteTable('locations', {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  age: int().notNull(),
+  email: text().notNull().unique(),
+})
